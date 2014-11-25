f41efcfd5a5595788c3b060b94e59d7d7e88b4a6
Zachary Siegel <zsiegel87@gmail.com>
2014-11-25 09:56:13 -0600

    rendering out a headers object if needed

--- RetrofitCodeGenerator.js
+++ RetrofitCodeGenerator.js
@@ -4,18 +4,56 @@ require("URI.js");
 
 var RetrofitCodeGenerator = function() {
 
+    this.headers = function(request) {
+
+        var headers = request.headers;
+        var result = {};
+        result.hasHeaders = Object.keys(headers).length > 0;
+        result.list = [];
+
+        for (headerName in headers) {
+            var headerValue = headers[headerName];
+            result.list.push({
+                "name" : headerName,
+                "value" : headerValue
+            });
+        }
+        return result;
+    }
+
+    this.renderHeader = function (headers) {
+        var headerView = {
+            "combine" : headers.list.length > 1,
+            "headers" : headers
+        }
+        return Mustache.render(readFile("headers.mustache"), headerView);
+    }
+
+    this.params = function(request) {
+
+        var result = [];
+        var queryParams = URI(request.url).search(true);
+        for (name in queryParams) {
+            var value = queryParams[name];
+            result.list.push({
+                "name": name,
+                "value": value
+            });
+        }
+    }
+
     this.generate = function(context) {
 
         var request = context.getCurrentRequest();
-
         var path = URI(request.url).path();
+        var headers = this.headers(request);
 
         // define your view
         var view = {
             "request":request,
             "path":path,
-            "content_type":context.getCurrentRequest().getHeaderByName('Content-Type'),
-            "method":context.getCurrentRequest().method
+            "headers":this.renderHeader(headers),
+            "method":request.method
         };
 
         // import the mustache template
